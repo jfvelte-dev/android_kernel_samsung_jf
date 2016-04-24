MiCode/Xiaomi_Kernel_OpenSource

msm: apr: Downgrade noisy informational prints from pr_info to pr_debug
Kernel logs frequently contain repetitive messages like the following,
which are not indications that anything has gone wrong. Quiet these
to decrease log noise and leave more room for error messages.

    <6>[  121.102540] apr_register: adsp Up
    <6>[  121.105663] apr_tal: SMD_EVENT_OPEN
    <6>[  121.108377] apr_register: adsp Up
    <6>[  121.122688] apr_register: adsp Up
    <6>[  121.131492] apr_register: adsp Up
    <6>[  126.775303] apr_register: adsp Up
    <6>[  126.778134] apr_tal: SMD_EVENT_OPEN
    <6>[  126.781267] apr_register: adsp Up
    <6>[  126.784916] apr_register: adsp Up

Change-Id: I9c3be65a1da547ead49b5f2b32820a047813c706
Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

arch/arm/mach-msm/qdsp6v2/apr_v2.c
@@ -50,16 +50,16 @@ struct apr_svc *apr_register(char *dest, char *svc_name, apr_fn svc_fn,
 			pr_err("%s: adsp not up\n", __func__);
 			return NULL;
 		}
-		pr_info("%s: adsp Up\n", __func__);
+		pr_debug("%s: adsp Up\n", __func__);
 	} else if ((dest_id == APR_DEST_MODEM) &&
 		   (apr_get_modem_state() == APR_SUBSYS_DOWN)) {
-		pr_info("%s: Wait for modem to bootup\n", __func__);
+		pr_debug("%s: Wait for modem to bootup\n", __func__);
 		rc = apr_wait_for_device_up(dest_id);
 		if (rc == 0) {
 			pr_err("%s: Modem is not Up\n", __func__);
 			return NULL;
 		}
-		pr_info("%s: modem Up\n", __func__);
+		pr_debug("%s: modem Up\n", __func__);
 	}
 
 	if (apr_get_svc(svc_name, dest_id, &client_id, &svc_idx, &svc_id)) {

jfvelte-dev/android_kernel_samsung_jf

arch/arm/mach-msm/qdsp6v2/apr_v2.c
@@ -48,16 +48,16 @@ struct apr_svc *apr_register(char *dest, char *svc_name, apr_fn svc_fn,
 			pr_err("%s: adsp not up\n", __func__);
 			return NULL;
 		}
-		pr_info("%s: Lpass Up\n", __func__);
+		pr_debug("%s: Lpass Up\n", __func__);
 	} else if ((dest_id == APR_DEST_MODEM) &&
 		   (apr_get_modem_state() == APR_SUBSYS_DOWN)) {
-		pr_info("%s: Wait for modem to bootup\n", __func__);
+		pr_debug("%s: Wait for modem to bootup\n", __func__);
 		rc = apr_wait_for_device_up(dest_id);
 		if (rc == 0) {
 			pr_err("%s: Modem is not Up\n", __func__);
 			return NULL;
 		}
-		pr_info("%s: modem Up\n", __func__);
+		pr_debug("%s: modem Up\n", __func__);
 	}
 
 	if (apr_get_svc(svc_name, dest_id, &client_id, &svc_idx, &svc_id)) {