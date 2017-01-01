a2d8280db94ec3c89d7ba500e2545f7b4723f4b0
sombree <sombree@gmail.com>
2017-01-01 23:34:19 +0100

    add some stuff

--- drivers/mfd/max77693.c
+++ drivers/mfd/max77693.c
@@ -167,7 +167,7 @@ static int max77693_i2c_probe(struct i2c_client *i2c,
 		pr_info("%s: device found: rev.0x%x, ver.0x%x\n", __func__,
 				max77693->pmic_rev, max77693->pmic_ver);
 	}
-#if 0
+// #if 0
 #if defined(CONFIG_MACH_JF_VZW) || defined(CONFIG_MACH_JF_LGT)
 #ifdef CONFIG_SEC_DEBUG
 	if (kernel_sec_get_debug_level() == KERNEL_SEC_DEBUG_LEVEL_LOW) {
@@ -191,7 +191,7 @@ static int max77693_i2c_probe(struct i2c_client *i2c,
 		max77693_write_reg(i2c, MAX77693_PMIC_REG_MAINCTRL1, 0x0c);
 	}
 #endif
-#endif
+// #endif
 
 #endif
 	max77693_update_reg(i2c, MAX77693_CHG_REG_SAFEOUT_CTRL, 0x00, 0x30);
